1e7a3846399d7a9fd3c4083996db6cd945c11c13
swd <swdchw@gmail.com>
2023-06-24 13:25:54 +0500

    uhm..

--- Patch/JailBirdPatch.cs
+++ Patch/JailBirdPatch.cs
@@ -17,9 +17,23 @@ namespace AutoEvent.Patch
             internal static void Prefix(JailbirdItem __instance, NetworkReader reader)
             {
                 if (AutoEvent.ActiveEvent == null) return;
+
+                if (AutoEvent.Singleton.Config.IsJailbirdHasInfinityCharges)
+                {
+                    __instance.TotalChargesPerformed = 0;
+                }
+
+
+                if (AutoEvent.Singleton.Config.IsJailbirdAbilityEnable) return;
+                __instance._chargeAutoengageTime = 99999;
+                __instance._chargeDetectionDelay = 99999;
+                __instance._chargeReadyTime = 99999;
                 __instance._chargeDuration = 0;
+                __instance._firstChargeFrame = true;
                 __instance._chargeLoading = false;
-                __instance._chargeResetTime = 99999f;
+                __instance._chargeAnyDetected = false;
+                __instance._chargeResetTime = 9999f;
+                __instance._chargeLoadElapsed = 0;
             }
         }
     }
