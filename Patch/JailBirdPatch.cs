b2a1b9982db94883c74ed45d494a09be67cf85c7
swd <swdchw@gmail.com>
2023-06-24 13:44:59 +0500

    true fix

--- Patch/JailBirdPatch.cs
+++ Patch/JailBirdPatch.cs
@@ -1,39 +1,37 @@
-﻿using HarmonyLib;
+﻿using Exiled.API.Features;
+using HarmonyLib;
 using InventorySystem.Items.Jailbird;
 using Mirror;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AutoEvent.Patch
 {
     internal class JailBirdPatch
     {
-        [HarmonyPatch(typeof(JailbirdItem), "ServerProcessCmd")]
-        static class Update
+        [HarmonyPatch(typeof(JailbirdItem), nameof(JailbirdItem.ServerProcessCmd))]
+        static class ServerProcessCmdPatch
         {
-            internal static void Prefix(JailbirdItem __instance, NetworkReader reader)
+            internal static bool Prefix(JailbirdItem __instance, NetworkReader reader)
             {
-                if (AutoEvent.ActiveEvent == null) return;
+                if (AutoEvent.Singleton.Config.IsJailbirdAbilityEnable)
+                {
 
+                    JailbirdMessageType jailbirdMessageType = (JailbirdMessageType)reader.ReadByte();
+
+                    switch (jailbirdMessageType)
+                    {
+                        case JailbirdMessageType.ChargeStarted:
+                        case JailbirdMessageType.ChargeLoadTriggered:
+                        case JailbirdMessageType.ChargeFailed:
+                            return false;
+                        default:
+                            return true;
+                    }
+                }
                 if (AutoEvent.Singleton.Config.IsJailbirdHasInfinityCharges)
                 {
                     __instance.TotalChargesPerformed = 0;
                 }
-
-
-                if (AutoEvent.Singleton.Config.IsJailbirdAbilityEnable) return;
-                __instance._chargeAutoengageTime = 99999;
-                __instance._chargeDetectionDelay = 99999;
-                __instance._chargeReadyTime = 99999;
-                __instance._chargeDuration = 0;
-                __instance._firstChargeFrame = true;
-                __instance._chargeLoading = false;
-                __instance._chargeAnyDetected = false;
-                __instance._chargeResetTime = 9999f;
-                __instance._chargeLoadElapsed = 0;
+                return true;
             }
         }
     }
