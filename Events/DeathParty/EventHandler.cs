434b2d823699de75c3b36dc4472a542afb968f13
Num1ock <71600441+Num10ck@users.noreply.github.com>
2023-06-28 20:38:19 +0400

    1

---
+++ Events/DeathParty/EventHandler.cs
@@ -0,0 +1,15 @@
+﻿using Exiled.API.Enums;
+using Exiled.Events.EventArgs.Player;
+using Exiled.Events.EventArgs.Server;
+using PlayerRoles;
+
+namespace AutoEvent.Events.DeathParty
+{
+    public class EventHandler
+    {
+        public void OnTeamRespawn(RespawningTeamEventArgs ev)
+        {
+            ev.IsAllowed = false;
+        }
+    }
+}
---
+++ Events/DeathParty/RandomRoles.cs
@@ -0,0 +1,19 @@
+﻿using PlayerRoles;
+using System.Collections.Generic;
+
+namespace AutoEvent.Events.DeathParty
+{
+    internal class RandomRoles
+    {
+        public static RoleTypeId GetRandomRole() => HumanRoles.RandomItem();
+
+        private static List<RoleTypeId> HumanRoles = new List<RoleTypeId>()
+        {
+            RoleTypeId.ClassD,
+            RoleTypeId.Scientist,
+            RoleTypeId.NtfSergeant,
+            RoleTypeId.ChaosRifleman,
+            RoleTypeId.FacilityGuard
+        };
+    }
+}
