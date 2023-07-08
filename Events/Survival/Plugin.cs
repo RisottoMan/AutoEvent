67d2ba76e03277fb897bb9b5e33f2e7e4f9b0a05
swd <swdchw@gmail.com>
2023-07-08 22:41:49 +0500

    СУКА ЕБАННЫЕ КОНФЛИКТЫ НАХУЙ

--- Events/Survival/Plugin.cs
+++ Events/Survival/Plugin.cs
@@ -1,4 +1,6 @@
 ﻿using AutoEvent.Interfaces;
+using CustomPlayerEffects;
+using Exiled.API.Enums;
 using Exiled.API.Features;
 using MapEditorReborn.API.Features.Objects;
 using MEC;
@@ -12,114 +14,142 @@ namespace AutoEvent.Events.Survival
 {
     public class Plugin : Event
     {
-        public override string Name { get; set; } = "Зомби Выживание [Testing]";
-        public override string Description { get; set; } = "Выживание людей против зомби. [Alpha]";
+        public override string Name { get; set; } = AutoEvent.Singleton.Translation.SurvivalName;
+        public override string Description { get; set; } = AutoEvent.Singleton.Translation.SurvivalDescription;
         public override string Color { get; set; } = "FF4242";
-        public override string CommandName { get; set; } = "survival";
+        public override string CommandName { get; set; } = "zombie2";
         public SchematicObject GameMap { get; set; }
         public TimeSpan EventTime { get; set; }
 
-        //EventHandler _eventHandler;
+        private bool isFriendlyFireEnabled;
+        EventHandler _eventHandler;
 
         public override void OnStart()
         {
-            //_eventHandler = new EventHandler();
-            /*
-            Exiled.Events.Handlers.Player.Verified += _eventHandler.OnJoin;
-            Exiled.Events.Handlers.Player.Died += _eventHandler.OnDead;
-            Exiled.Events.Handlers.Player.Hurting += _eventHandler.OnDamage;
-            Exiled.Events.Handlers.Server.RespawningTeam += _eventHandler.OnTeamRespawn;
-            */
+            isFriendlyFireEnabled = Server.FriendlyFire;
+            Server.FriendlyFire = false;
+
             OnEventStarted();
+
+            _eventHandler = new EventHandler(this);
+
+            Exiled.Events.Handlers.Player.Verified += _eventHandler.OnJoin;
+            Exiled.Events.Handlers.Server.RespawningTeam += _eventHandler.OnTeamRespawn;
+            Exiled.Events.Handlers.Player.SpawningRagdoll += _eventHandler.OnSpawnRagdoll;
+            Exiled.Events.Handlers.Player.Spawned += _eventHandler.OnSpawned;
+            Exiled.Events.Handlers.Map.PlacingBulletHole += _eventHandler.OnPlaceBullet;
+            Exiled.Events.Handlers.Map.PlacingBlood += _eventHandler.OnPlaceBlood;
+            Exiled.Events.Handlers.Player.ReloadingWeapon += _eventHandler.OnReloading;
+            Exiled.Events.Handlers.Player.DroppingItem += _eventHandler.OnDropItem;
+            Exiled.Events.Handlers.Player.DroppingAmmo += _eventHandler.OnDropAmmo;
+            Exiled.Events.Handlers.Player.Hurting += _eventHandler.OnDamage;
+            Exiled.Events.Handlers.Player.Died += _eventHandler.OnDead;
         }
         public override void OnStop()
         {
-            /*
+            Server.FriendlyFire = isFriendlyFireEnabled;
+
             Exiled.Events.Handlers.Player.Verified -= _eventHandler.OnJoin;
-            Exiled.Events.Handlers.Player.Died -= _eventHandler.OnDead;
-            Exiled.Events.Handlers.Player.Hurting -= _eventHandler.OnDamage;
             Exiled.Events.Handlers.Server.RespawningTeam -= _eventHandler.OnTeamRespawn;
-            */
+            Exiled.Events.Handlers.Player.SpawningRagdoll -= _eventHandler.OnSpawnRagdoll;
+            Exiled.Events.Handlers.Player.Spawned -= _eventHandler.OnSpawned;
+            Exiled.Events.Handlers.Map.PlacingBulletHole -= _eventHandler.OnPlaceBullet;
+            Exiled.Events.Handlers.Map.PlacingBlood -= _eventHandler.OnPlaceBlood;
+            Exiled.Events.Handlers.Player.ReloadingWeapon -= _eventHandler.OnReloading;
+            Exiled.Events.Handlers.Player.DroppingItem -= _eventHandler.OnDropItem;
+            Exiled.Events.Handlers.Player.DroppingAmmo -= _eventHandler.OnDropAmmo;
+            Exiled.Events.Handlers.Player.Hurting -= _eventHandler.OnDamage;
+            Exiled.Events.Handlers.Player.Died -= _eventHandler.OnDead;
+
+            _eventHandler = null;
             Timing.CallDelayed(10f, () => EventEnd());
-            AutoEvent.ActiveEvent = null;
-            //_eventHandler = null;
-        }
-        public void OnEventStarted()
-        {
-            // Обнуление Таймера
-            EventTime = new TimeSpan(0, 5, 0);
-            // Создание карты
-            GameMap = Extensions.LoadMap("Zm_Dust_World.json", new Vector3(115.5f, 1035f, -43.5f), new Quaternion(0, 0, 0, 0), new Vector3(1, 1, 1));
-            Extensions.PlayAudio("Survival.ogg", 5, false, "Выживание");
-            // Телепорт игроков
-            foreach (Player player in Player.List)
-            {
-                player.Role.Set(RoleTypeId.NtfSergeant, Exiled.API.Enums.SpawnReason.None, RoleSpawnFlags.AssignInventory);
-                //player.Position = GameMap.Position + RandomClass.GetRandomSpawn();
-            }
-            Timing.RunCoroutine(TimingBeginEvent($"Выживание", 15), "survival_run");
-        }
-        // Отсчет до начала ивента
-        public IEnumerator<float> TimingBeginEvent(string eventName, float time)
-        {
-            for (float _time = time; _time > 0; _time--)
-            {
-                Map.ClearBroadcasts();
-                Map.Broadcast(1, $"<color=#D71868><b><i>{eventName}</i></b></color>\n<color=#ABF000>До начала ивента осталось <color=red>{_time}</color> секунд.</color>");
-                yield return Timing.WaitForSeconds(1f);
-            }
-            SpawnZombie();
-            yield break;
         }
 
-        public void SpawnZombie()
+        public void OnEventStarted()
         {
-            for(int i = 0; i <= Player.List.Count() / 10; i++)
+            EventTime = new TimeSpan(0, 5, 0);
+
+            GameMap = Extensions.LoadMap("Survival", new Vector3(15f, 1030f, -43.68f), Quaternion.identity, Vector3.one);
+            Extensions.PlayAudio("Survival.ogg", 10, false, Name);
+
+            foreach (Player player in Player.List)
             {
-                Player.List.ToList().RandomItem().Role.Set(RoleTypeId.Scp0492, Exiled.API.Enums.SpawnReason.Revived, RoleSpawnFlags.AssignInventory);
+                player.Role.Set(RoleTypeId.NtfSergeant, SpawnReason.None, RoleSpawnFlags.AssignInventory);
+                player.Position = RandomClass.GetSpawnPosition(GameMap);
+                player.AddAhp(100, 100, 0, 0, 0, true);
+
+                Timing.CallDelayed(0.1f, () =>
+                {
+                    player.CurrentItem = player.Items.ElementAt(1);
+                });
             }
-            Timing.RunCoroutine(OnEventRunning(), "infect_run");
+
+            Timing.RunCoroutine(OnEventRunning(), "survival_run");
         }
 
         public IEnumerator<float> OnEventRunning()
         {
-            while (Player.List.Count(r => r.Role == RoleTypeId.NtfSergeant) > 0 && Player.List.Count(r => r.Role == RoleTypeId.Scp0492) > 0 && EventTime.TotalSeconds > 0)
+            for (float _time = 20; _time > 0; _time--)
             {
-                Map.ClearBroadcasts();
-                Map.Broadcast(1, $"<color=#D71868><b><i>Зомби Выживание</i></b></color>\n" +
-                    $"<color=yellow>Осталось людей: <color=green>{Player.List.Count(r => r.Role == RoleTypeId.NtfSergeant)}</color></color>\n" +
-                    $"<color=yellow>Время до конца: <color=red>{EventTime.Minutes}:{EventTime.Seconds}</color></color>");
+                Extensions.Broadcast(AutoEvent.Singleton.Translation.SurvivalBeforeInfection.Replace("%name%", Name).Replace("%time%", $"{_time}"), 1);
+                yield return Timing.WaitForSeconds(1f);
+            }
+
+            for (int i = 0; i <= Player.List.Count() / 10; i++)
+            {
+                var player = Player.List.Where(r => r.IsHuman).ToList().RandomItem();
+                player.Role.Set(RoleTypeId.Scp0492, SpawnReason.None, RoleSpawnFlags.AssignInventory);
+                player.EnableEffect<Disabled>();
+                player.EnableEffect<Scp1853>();
+                player.Health = 10000;
+            }
+
+            var teleport = GameMap.AttachedBlocks.First(x => x.name == "Teleport");
+            var teleport1 = GameMap.AttachedBlocks.First(x => x.name == "Teleport1");
+
+            while (Player.List.Count(r => r.IsHuman) > 0 && Player.List.Count(r => r.IsScp) > 0 && EventTime.TotalSeconds > 0)
+            {
+
+                foreach (var player in Player.List)
+                {
+                    player.ClearBroadcasts();
+                    player.Broadcast(1, AutoEvent.Singleton.Translation.SurvivalAfterInfection.Replace("%name%", Name).Replace("%humanCount%", $"{Player.List.Count(r => r.IsHuman)}")
+                            .Replace("%time%", $"{EventTime.Minutes}:{EventTime.Seconds}"));
+
+                    if (Vector3.Distance(player.Position, teleport.transform.position) < 1)
+                    {
+                        player.Position = teleport1.transform.position;
+                    }
+                }
 
                 yield return Timing.WaitForSeconds(1f);
                 EventTime -= TimeSpan.FromSeconds(1f);
             }
-            if (Player.List.Count(r => r.Role == RoleTypeId.NtfSergeant) == 0)
+
+            if (Player.List.Count(r => r.IsHuman) == 0)
             {
-                Map.ClearBroadcasts();
-                Map.Broadcast(10, $"<color=red>Зомби Победили!</color>\n" +
-                $"<color=yellow>Зомби всех заразили</color>");
+                Extensions.Broadcast(AutoEvent.Singleton.Translation.SurvivalZombieWin, 10);
             }
-            else if (Player.List.Count(r => r.Role == RoleTypeId.Scp0492) == 0)
+            else if (Player.List.Count(r => r.IsScp) == 0)
             {
-                Map.ClearBroadcasts();
-                Map.Broadcast(10, $"<color=yellow><color=#D71868><b><i>Люди</i></b></color> Победили!</color>\n" +
-                $"<color=yellow>Люди остановили чуму и убили всех зомби</color>");
+                Extensions.Broadcast(AutoEvent.Singleton.Translation.SurvivalHumanWin, 10);
             }
             else
             {
-                Map.ClearBroadcasts();
-                Map.Broadcast(10, $"<color=yellow><color=#D71868><b><i>Люди</i></b></color> Победили!</color>\n" +
-                $"<color=yellow>Люди выжили, но это ещё не конец</color>");
+                Extensions.Broadcast(AutoEvent.Singleton.Translation.SurvivalHumanWinTime, 10);
             }
+
             OnStop();
             yield break;
         }
+
         public void EventEnd()
         {
             Extensions.CleanUpAll();
             Extensions.TeleportEnd();
             Extensions.UnLoadMap(GameMap);
             Extensions.StopAudio();
+            AutoEvent.ActiveEvent = null;
         }
     }
 }
