3d4e8529d756c766bdb4c0262395e74d7e4c5267
Logic_Gun <logicgunwhat@bk.ru>
2023-07-14 22:50:52 +0500

    Add RandItems

---
+++ Events/RandItems/EventHandler.cs
@@ -0,0 +1,21 @@
+﻿using Exiled.API.Enums;
+using Exiled.Events.EventArgs.Cassie;
+using Exiled.Events.EventArgs.Player;
+using Exiled.Events.EventArgs.Scp173;
+using Exiled.Events.EventArgs.Server;
+using PlayerRoles;
+
+namespace AutoEvent.Events.RandItems
+{
+    public class EventHandler
+    {
+        public void OnJoin(VerifiedEventArgs ev)
+        {
+            ev.Player.Role.Set(RoleTypeId.Scp173, SpawnReason.None, RoleSpawnFlags.All);
+        }
+        public void OnTeamRespawn(RespawningTeamEventArgs ev)
+        {
+            ev.IsAllowed = false;
+        }
+    }
+}
--- Translation.cs
+++ Translation.cs
@@ -109,9 +109,12 @@ namespace AutoEvent
         [Description("Fall Down Game Mode")] 
         public string FallName { get; set; } = "FallDown";
         public string FallDescription { get; set; } = "All platforms are destroyed. It is necessary to survive";
-
         public string FallBroadcast { get; set; } = "%name%\n%time%\n<color=yellow>Remaining: </color>%count%<color=yellow> players</color>";
         public string FallWinner { get; set; } = "<color=red>Winner:</color> %winner%";
         public string FallDied { get; set; } = "<color=red>All players died</color>";
+        [Description("RandItems Game Mode")]
+        public string RandName { get; set; } = "RandItems";
+        public string RandDescription { get; set; } = "Every 3 minutes a random item as given";
+        public string RandDied { get; set; } = "<color=red>All players died</color>";
     }
 }
