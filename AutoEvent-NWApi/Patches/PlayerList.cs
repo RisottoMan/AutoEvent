07d3f1e5ef17799f01c71d406c6bce9d92716c60
KoT0XleB <lapka.roman@yandex.ru>
2023-08-24 21:52:35 +0500

    damn playerlist patch

--- AutoEvent-NWApi/Patches/PlayerList.cs
+++ AutoEvent-NWApi/Patches/PlayerList.cs
@@ -1,29 +1,17 @@
 ﻿using HarmonyLib;
 using PluginAPI.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace AutoEvent.Patches
 {
-    //[HarmonyPatch(typeof(Player), nameof(Player.GetPlayers))]
+    [HarmonyPatch(typeof(Player), nameof(Player.GetPlayers))]
     public class PlayerList
     {
-        // надо исправить этот патч. Он должен убирать из списка игроков нашего бота, чтобы его не спавнило
-        // вся игровая логика проверяется через этот метод, поэтому убрать бота отсюда логично
-        public static bool Prefix(Player __instance, ref List<Player> __result) // попробовать переписать???
+        public static bool Prefix<Player>(Player __instance , ref List<Player> __result)
         {
-            List<Player> newList = new List<Player>();
-
-            foreach (ReferenceHub allHub in ReferenceHub.AllHubs)
-            {
-                if (!allHub.isLocalPlayer || !Extensions.IsAudioBot(allHub))
-                {
-                    newList.Add(Player.Get(allHub));
-                }
-            }
-
-            __result = newList;
-
+            __result = new List<Player>();
             return false;
         }
     }
