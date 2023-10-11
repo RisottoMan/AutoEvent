64b96e38a41242472d4eee97a5cd9617d69b0ce4
KoT0XleB <lapka.roman@yandex.ru>
2023-10-11 14:57:24 +0500

    add lobby and vote
    1) Added a lobby as a mini-game.
    2) Added a vote as a mini-game.
    3) Added a map lobby.json for the lobby.
    4) Added music for the lobby and vote.
    5) Added the PlayerNoclip event, which reads pressing the Alt button.

---
+++ AutoEvent/Games/Lobby/EventHandler.cs
@@ -0,0 +1,19 @@
+﻿using PlayerRoles;
+using PluginAPI.Events;
+
+namespace AutoEvent.Games.Lobby
+{
+    public class EventHandler
+    {
+        Plugin _plugin;
+        public EventHandler(Plugin plugin)
+        {
+            _plugin = plugin;
+        }
+
+        public void OnPlayerJoined(PlayerJoinedEvent ev)
+        {
+            ev.Player.SetRole(RoleTypeId.Spectator);
+        }
+    }
+}
