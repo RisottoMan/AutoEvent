e85b7c3327a33875672dd0ef86ab0ed9f34ae240
Redforce04 <74625280+Redforce04@users.noreply.github.com>
2023-10-17 18:17:35 -0500

    still doesnt work - womp womp

--- AutoEvent/API/Rock.cs
+++ AutoEvent/API/Rock.cs
@@ -11,6 +11,7 @@
 // -----------------------------------------
 
 using System;
+using AutoEvent.API.Components;
 using AutoEvent.Games.Glass.Features;
 using Footprinting;
 using Interactables.Interobjects;
@@ -78,45 +79,63 @@ public class Rock : MonoBehaviour
 
     public void Awake()
     {
+        var collider = gameObject.AddComponent<PlayerCollisionDetectorComponent>();
+        collider.PlayerCollision += PlayerCollision;
         if (gameObject.TryGetComponent<Rock>(out _))
             Destroy(this);
     }
 
-
-    public void OnCollisionEnter(Collision collision)
+    public void PlayerCollision(PlayerCollisionArgs ev)
     {
         try
         {
-            if (collision.gameObject == Owner)
+            var args = _processRockHitArgs(ev.Player.GameObject);
+            if (args is null)
             {
                 return;
             }
+            _giveNewRock(args);
+            _destroy();
+        }
+        catch (Exception e)
+        {
+            DebugLogger.LogDebug($"Caught an exception on player collision.", LogLevel.Warn, true);
+            DebugLogger.LogDebug($"{e}");
+        }
+    }
 
-            var args = new RockHitPlayerArgs(Thrower, collision, ThrowDamage, FriendlyFire, ExplodeOnCollision, true, gameObject.layer);
-            OnRockHitPlayer(ref args);
-            if (!args.IsAllowed)
-            {
-                return;
-            }
-            if (args.Target is not null && args.Target.Team == args.Thrower.Role.GetTeam() && args.FriendlyFire)
-            {
-                args.IsAllowed = false;
-            }
-            ProcessDefaultDamage(args);
-            if (args.ExplodeOnCollision)
-            {
-                Vector3 position = collision.transform.position;
-                ExplosionUtils.ServerExplode(collision.GetContact(0).point, Thrower);
-            }
+    private RockHitObjectArgs _processRockHitArgs(GameObject gameObject)
+    {
+        var args = new RockHitObjectArgs(Thrower, gameObject, ThrowDamage, FriendlyFire, ExplodeOnCollision, true, gameObject.layer);
+        OnRockHitPlayer(ref args);
+        if (!args.IsAllowed)
+        {
+            return null;
+        }
+        if (args.Target is not null && args.Target.Team == args.Thrower.Role.GetTeam() && args.FriendlyFire)
+        {
+            args.IsAllowed = false;
+        }
+        ProcessDefaultDamage(args);
+        if (args.ExplodeOnCollision)
+        {
+            Vector3 position = gameObject.transform.position;
+            ExplosionUtils.ServerExplode(position, Thrower);
+        }
 
-            // ReSharper disable once ConditionIsAlwaysTrueOrFalse
-            if (LeaveBehindRock && DebugLogger.Debug)
+        return args;
+    }
+
+    private void _giveNewRock(RockHitObjectArgs args)
+    {
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalse
+            if (LeaveBehindRock )//&& DebugLogger.Debug)
             {
-                DebugLogger.LogDebug("Leave behind rock is a beta feature only enabled in debug mode...");
+                //DebugLogger.LogDebug("Leave behind rock is a beta feature only enabled in debug mode...");
                 // this doesnt work :(
                 
                 var throwable = Extensions.CreateThrowable(ItemType.SCP018);
-                TimeGrenade grenade = (TimeGrenade) Object.Instantiate(throwable.Projectile, collision.GetContact(0).point, Quaternion.identity);
+                TimeGrenade grenade = (TimeGrenade) Object.Instantiate(throwable.Projectile, gameObject.transform.position, Quaternion.identity);
                 grenade.PreviousOwner = new Footprint(args.Thrower.Hub != null ? args.Thrower.Hub : ReferenceHub.HostHub);
                 NetworkServer.Spawn(grenade.gameObject);
                 if (grenade.TryGetComponent<Scp018Projectile>(out Scp018Projectile ball))
@@ -124,7 +143,7 @@ public class Rock : MonoBehaviour
                     // delete the scp018 component.
                     Destroy(ball);
                 }
-                DebugLogger.LogDebug($"spawning new Scp018");
+                //DebugLogger.LogDebug($"spawning new Scp018");
                 ((Scp018Projectile)grenade).PhysicsModule.DestroyModule();
                 
                 ((Scp018Projectile)grenade).PhysicsModule = new PickupStandardPhysics(((Scp018Projectile)grenade), PickupStandardPhysics.FreezingMode.FreezeWhenSleeping) { _isFrozen = true, _serverPrevSleeping = true };
@@ -156,9 +175,30 @@ public class Rock : MonoBehaviour
                     // full inv
                 }
             }
-            Destroy(Owner.gameObject);
-            Destroy(this);
-            Destroy(gameObject);
+    }
+
+    private void _destroy()
+    {
+        Destroy(Owner.gameObject);
+        Destroy(this);
+        Destroy(gameObject);
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        try
+        {
+            if (collision.gameObject == Owner)
+            {
+                return; 
+            }
+
+            var args = _processRockHitArgs(collision.contacts[0].otherCollider.gameObject);
+            if (args is null)
+            {
+                return;
+            }
+            _giveNewRock(args);
+            _destroy();
         }
         catch (Exception e)
         {
@@ -167,7 +207,7 @@ public class Rock : MonoBehaviour
     }
 
     
-    public void ProcessDefaultDamage(RockHitPlayerArgs ev)
+    public void ProcessDefaultDamage(RockHitObjectArgs ev)
     {
         if (!ev.IsAllowed)
         {
@@ -190,7 +230,7 @@ public class Rock : MonoBehaviour
 
             if (window is not null)
             {
-                window.Damage(ev.Damage, new CustomReasonDamageHandler("Rock"), ev.Collision.GetContact(0).point);
+                window.Damage(ev.Damage, new CustomReasonDamageHandler("Rock"), ev.TargetObject.transform.position);
             }
 
             if (door is not null)
@@ -218,176 +258,43 @@ public class Rock : MonoBehaviour
     public float ThrowDamage { get; set; }
     public bool LeaveBehindRock { get; set; }
     public bool GiveOwnerNewRockOnHit { get; set; }
-    public void OnRockHitPlayer(ref RockHitPlayerArgs ev) => RockHitPlayerEvent?.Invoke(ev);
-    public event Action<RockHitPlayerArgs> RockHitPlayerEvent;
+    public void OnRockHitPlayer(ref RockHitObjectArgs ev) => RockHitPlayerEvent?.Invoke(ev);
+    public event Action<RockHitObjectArgs> RockHitPlayerEvent;
 
 
 }
 
-public class RockHitPlayerArgs
+public class RockHitObjectArgs
 {
-    public RockHitPlayerArgs(Footprint footprint, Collision collision, float damage, bool friendlyFire, bool explodeOnCollision, bool isAllowed = true, int layerMask = -1)
+    public RockHitObjectArgs(Footprint footprint, GameObject collision, float damage, bool friendlyFire,
+        bool explodeOnCollision, bool isAllowed = true, int layerMask = -1)
     {
         LayerMask = layerMask;
         IsAllowed = isAllowed;
-        Collision = collision;
         Damage = damage;
         FriendlyFire = friendlyFire;
         ExplodeOnCollision = explodeOnCollision;
         Thrower = footprint;
-        TargetObject = collision.collider.gameObject;
-        var refHub = collision.collider.GetComponentInParent<ReferenceHub>();
+        TargetObject = collision;
+        //var refHub = collision.GetComponentInParent<ReferenceHub>();
+        Target = Player.Get(collision);
         
-        if (refHub is not null)
-        {
-            Target = Player.Get(refHub);
-            DebugLogger.LogDebug("Target player for rock found.");
-            return;
-        }
-        var gameObject = collision.collider.transform.root.gameObject;
-        Player ply = Player.Get(gameObject);
-        if (ply is not null)
+        if (Target is not null)
         {
             DebugLogger.LogDebug("Target player for rock found.");
-            Target = ply;
             return;
         }
-
-        if (!DebugLogger.Debug)
-            return;
-        ReferenceHub hub = Collision.collider.GetComponentInParent<ReferenceHub>();
-        int a = Collision.GetContact(0).thisCollider.gameObject.layer;
-        int b = Collision.GetContact(0).otherCollider.gameObject.layer;
-        DebugLogger.LogDebug($"A: {a}, B: {b}");
         
-
-            /*foreach (var transform in collider.GetComponentsInParent<ReferenceHub>())
-            {
-                foreach (var obj in transform.GetComponentsInParent<Component>())
-                {
-                    DebugLogger.LogDebug($"Component: {obj.GetType().Name}{obj.name}");
-                }
-            }*/
-            
-            /*if (collider.TryGetComponent<IDestructible>(out IDestructible destructible))
-            {
-                DebugLogger.LogDebug($"Found Destructible");
-            }
-
-            if (collider.TryGetComponent<HitboxIdentity>(out HitboxIdentity identity))
-            {
-                DebugLogger.LogDebug($"Found Hitbox");
-            }
-            */
-        /*DebugLogger.LogDebug("============ Direct Components ============");
-            foreach (var x in collider.GetComponents<Component>())
-            {
-                DebugLogger.LogDebug($"{x.GetType().Name}");
-            }
-            DebugLogger.LogDebug("============ Parent Components ============");
-            foreach (var x in collider.GetComponentsInParent<Component>())
-            {
-                DebugLogger.LogDebug($"{x.GetType().Name}");
-            }*/
-            /*var refHub = collision.collider.GetComponentInParent<ReferenceHub>();
-        if (refHub is not null)
-        {
-            var player x = Player.Get(refHub);
-        }*/
-        DebugLogger.LogDebug($"HitboxIdentity: {collision.collider.GetComponentInParent<HitboxIdentity>() is not null}");
-        DebugLogger.LogDebug($"IDestructible: {collision.collider.GetComponentInParent<IDestructible>() is not null}");
-        DebugLogger.LogDebug($"RefHub: {collision.collider.GetComponentInParent<ReferenceHub>() is not null}");
-        DebugLogger.LogDebug($"HitboxIdentity: {collision.contacts[0].otherCollider.GetComponentInParent<HitboxIdentity>() is not null}");
-        DebugLogger.LogDebug($"IDestructible: {collision.contacts[0].otherCollider.GetComponentInParent<IDestructible>() is not null}");
-        DebugLogger.LogDebug($"RefHub: {collision.contacts[0].otherCollider.GetComponentInParent<ReferenceHub>() is not null}");
-
-        
-        int i = 0;
-        // foreach (var collider in collision.contacts)
-        foreach (var collider in Physics.OverlapSphere(collision.GetContact(0).point, 1f, layerMask == -1 ? 8 : layerMask))
-        {
-            //DebugLogger.LogDebug($"User: {collider.GetComponentInParent<ReferenceHub>().nicknameSync.DisplayName}");
-           // DebugLogger.LogDebug($"User: {collider.GetComponentInParent<ReferenceHub>().nicknameSync.DisplayName}");
-            
-            
-            
-            DebugLogger.LogDebug($"RefHub: {collider.TryGetComponent<ReferenceHub>(out _)}");
-            DebugLogger.LogDebug($"SafeTeleportPosition: {collider.TryGetComponent<SafeTeleportPosition>(out _)}");
-            DebugLogger.LogDebug($"Transform: {collider.TryGetComponent<Transform>(out _)}");
-            DebugLogger.LogDebug($"IDestructible: {collider.TryGetComponent<IDestructible>(out _)}");
-            DebugLogger.LogDebug($"HitboxIdentity: {collider.TryGetComponent<HitboxIdentity>(out _)}");
-            DebugLogger.LogDebug($"HitboxIdentity: {collider.GetComponentInParent<HitboxIdentity>() is not null}");
-            DebugLogger.LogDebug($"IDestructible: {collider.GetComponentInParent<IDestructible>() is not null}");
-            DebugLogger.LogDebug($"RefHub: {collider.GetComponentInParent<ReferenceHub>() is not null}");
-            DebugLogger.LogDebug($"SafeTeleportPosition: {collider.GetComponentInParent<SafeTeleportPosition>() is not null}");
-            DebugLogger.LogDebug($"Transform: {collider.GetComponentInParent<Transform>() is not null}");
-            foreach (var transform in collider.GetComponents<Transform>())
-            {
-                DebugLogger.LogDebug($"Transform: {transform.transform.position}");
-            }
-            foreach (var transform in collider.GetComponentsInParent<Transform>())
-            {
-                DebugLogger.LogDebug($"Parent Transform: {transform.transform.position}");
-            }
-            foreach (var destructible in collider.GetComponents<IDestructible>())
-            {
-                DebugLogger.LogDebug($"IDestructible: {destructible.NetworkId}");
-            }
-            foreach (var destructible in collider.GetComponentsInParent<IDestructible>())
-            {
-                DebugLogger.LogDebug($"Parent IDestructible: {destructible.NetworkId}");
-            }foreach (var hitbox in collider.GetComponents<HitboxIdentity>())
-            {
-                DebugLogger.LogDebug($"Hitbox: {hitbox.NetworkId}");
-            }
-            foreach (var hitbox in collider.GetComponentsInParent<HitboxIdentity>())
-            {
-                DebugLogger.LogDebug($"Parent Hitbox: {hitbox.NetworkId}");
-            }
-            
-            foreach (var tpPos in collider.GetComponents<SafeTeleportPosition>())
-            {
-                DebugLogger.LogDebug($"TP Pos: {tpPos.SafePositions.Length}");
-            }
-            foreach (var tpPos in collider.GetComponentsInParent<SafeTeleportPosition>())
-            {
-                DebugLogger.LogDebug($"Parent Tp Pos: {tpPos.SafePositions.Length}");
-            }
-
-
-            DebugLogger.LogDebug($"============ Direct Components (collider {i}) ============");
-            foreach (var x in collider.GetComponents<Component>())
-            {
-                DebugLogger.LogDebug($"{x.GetType().Name}");
-            }
-            DebugLogger.LogDebug($"============ Parent Components (collider {i}) ============");
-            foreach (var x in collider.GetComponentsInParent<Component>())
-            {
-                DebugLogger.LogDebug($"{x.GetType().Name}");
-            }
-
-            i++;
-        }
-        
-            
-#if EXILED
-        //var exPly3 = Exiled.API.Features.Player.Get(collision.gameObject.transform);
-        var exPly1 = Exiled.API.Features.Player.Get(collision.gameObject);
-        var exPly2 = Exiled.API.Features.Player.Get(collision.collider);
-        DebugLogger.LogDebug($"Player: {exPly1 is not null}, {exPly2 is not null} ");
-#endif
         DebugLogger.LogDebug("Could not get target player for rock.");
     }
 
     public int LayerMask { get; private set; }
     public Footprint Thrower { get; private set; }
     
-    public Collision Collision { get; private set; }
     
     [CanBeNull] 
     public Player Target { get; private set; }
     
-    [CanBeNull]
     public GameObject TargetObject { get; private set; }
     public bool IsAllowed { get; set; }
     public float Damage { get; set; }
