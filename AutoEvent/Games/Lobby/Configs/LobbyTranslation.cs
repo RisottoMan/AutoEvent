2b84ee218201730457c63baa95fe01f8aeb2ad52
Logic_Gun <logicgunwhat@bk.ru>
2023-11-16 18:49:12 +0500

    Edit Coloring in Vote, Lobby

--- AutoEvent/Games/Lobby/Configs/LobbyTranslation.cs
+++ AutoEvent/Games/Lobby/Configs/LobbyTranslation.cs
@@ -11,11 +11,11 @@ namespace AutoEvent.Games.Lobby
         public string LobbyCommandName { get; set; } = "lobby";
         public string LobbyName { get; set; } = "Lobby";
         public string LobbyDescription { get; set; } = "A lobby in which one quick player chooses a mini-game.";
-        public string LobbyCycle { get; set; } = "Vote: Press [Alt] Pros or [Alt]x2 Cons\n{trueCount} of {allCount} players for {newName}\n{time} seconds left!";
-        public string LobbyGlobalMessage { get; set; } = "Lobby\n{message}\n{count} players in the lobby";
-        public string LobbyGetReady { get; set; } = "Get ready to run to the center and choose a mini game";
-        public string LobbyRun { get; set; } = "RUN";
-        public string LobbyChoosing { get; set; } = "Waiting for the {nickName} to choose mini-game";
-        public string LobbyFinishMessage { get; set; } = "The lobby is finished.\nThe player {nickName} chose the {newName} mini-game.\nTotal {count} players in the lobby";
+        public string LobbyCycle { get; set; } = "<color=#1378f2>Vote: Press [Alt] Pros or [Alt]x2 Cons</color>\n<color=yellow>{trueCount}</color> <color=#1378f2>of</color> <color=yellow>{allCount}</color> <color=#1378f2>players for {newName}</color>\n<color=yellow>{time}</color> <color=#1378f2>seconds left!</color>";
+        public string LobbyGlobalMessage { get; set; } = "<color=#f03405>Lobby</color>\n<color=#1378f2>{message}</color>\n<color=yellow>{count}</color> <color=#f03405>players in the lobby</color>";
+        public string LobbyGetReady { get; set; } = "<b><color=yellow>Get ready to run to the center and choose a mini game</color></b>";
+        public string LobbyRun { get; set; } = "<color=red>RUN</color>";
+        public string LobbyChoosing { get; set; } = "<b><color=#1378f2>Waiting for the {nickName} to choose mini-game</color></b>";
+        public string LobbyFinishMessage { get; set; } = "<color=#1378f2>The lobby is finished.</color>\n<color=#1378f2>The player {nickName} chose the {newName} mini-game.</color>\n<color=#f03405>Total</color> <color=yellow>{count}</color> <color=#f03405>players in the lobby</color>";
     }
 }
--- AutoEvent/Games/Vote/Configs/VoteTranslation.cs
+++ AutoEvent/Games/Vote/Configs/VoteTranslation.cs
@@ -11,10 +11,10 @@ namespace AutoEvent.Games.Vote
         public string VoteCommandName { get; set; } = "vote";
         public string VoteName { get; set; } = "Vote";
         public string VoteDescription { get; set; } = "Start voting for the mini-game.";
-        public string VoteBeforeStart { get; set; } = "<color=#D71868><b><i>Run Forward</i></b></color>\nInfection starts in: {time}";
-        public string VoteCycle { get; set; } = "Vote: Press [Alt] Pros or [Alt]x2 Cons\n{trueCount} of {allCount} players for {newName}\n{time} seconds left!";
-        public string VoteMinigameStart { get; set; } = "{newName} will start soon.";
-        public string VoteMinigameNotStart { get; set; } = "{newName} will not start.";
-        public string VoteEnd { get; set; } = "Vote: End of voting\n{trueCount} of {allCount} players\n{results}";
+        public string VoteBeforeStart { get; set; } = "<color=#D71868><b><i>Run Forward</i></b></color>\nInfection starts in: <color=yellow>{time}</color>";
+        public string VoteCycle { get; set; } = "<color=#1378f2>Vote: Press [Alt] Pros or [Alt]x2 Cons</color>\n<color=yellow>{trueCount}</color> <color=#1378f2>of</color> <color=yellow>{allCount}</color> <color=#1378f2>players for {newName}</color>\n<color=yellow>{time}</color> <color=#1378f2>seconds left!</color>";
+        public string VoteMinigameStart { get; set; } = "<b><color=yellow>{newName} will start soon.</color></b>";
+        public string VoteMinigameNotStart { get; set; } = "<b><color=red>{newName} will not start.</color></b>";
+        public string VoteEnd { get; set; } = "<color=#1378f2>Vote: End of voting</color>\n<color=yellow>{trueCount}</color> <color=#1378f2>of</color> <color=yellow>{allCount}</color> <color=#1378f2>players</color>\n<color=yellow>{results}</color>";
     }
 }
