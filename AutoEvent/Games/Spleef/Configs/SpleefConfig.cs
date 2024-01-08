c181160b1748af91a3ba7d9c2aff416ba636d27f
KoT0XleB <lapka.roman@yandex.ru>
2024-01-08 18:09:07 +0500

    rewrite spleef and edit fody

--- AutoEvent/Games/Spleef/Configs/SpleefConfig.cs
+++ AutoEvent/Games/Spleef/Configs/SpleefConfig.cs
@@ -22,7 +22,18 @@ public class SpleefConfig : EventConfig
 {
     [Description("How long the round should last.")]
     public int RoundDurationInSeconds { get; set; } = 300;
-    
+
+    [Description("Set to -1 to disable. Causes platforms to regenerate after a certain number of seconds.")]
+    public int RegeneratePlatformsAfterXSeconds { get; set; } = -1;
+
+    [Description("How many platforms on the x and y axis. Total Platforms = PlatformAxisCount * PlatformAxisCount * LayerCount. Size is automatically determined.")]
+    public int PlatformAxisCount { get; set; } = 20;
+    [Description("How many \"levels\" of height should be in the map. ")]
+    public int LayerCount { get; set; } = 4;
+
+    [Description("How long before platforms will fall after being stepped on. Set to -1 to disable this.")]
+    public float PlatformFallDelay { get; set; } = -1;
+
     [Description("The amount of health platforms have. Set to -1 to make them invincible.")]
     public float PlatformHealth { get; set; } = 1;
 
