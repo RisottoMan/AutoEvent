0248b5604741d558b978924ec3b2ee10a4ff9ea7
KoT0XleB <96589180+KoT0XleB@users.noreply.github.com>
2024-02-10 12:13:02 +0500

    Update EventTranslation.cs

--- AutoEvent/Interfaces/EventTranslation.cs
+++ AutoEvent/Interfaces/EventTranslation.cs
@@ -1,7 +1,6 @@
 ﻿using System.ComponentModel;
 
 namespace AutoEvent.Interfaces;
-
 public abstract class EventTranslation : IEventTranslation
 {
     public EventTranslation()
@@ -14,5 +13,6 @@ public abstract class EventTranslation : IEventTranslation
     public abstract string CommandName { get; set; }
 
     [Description("DO NOT CHANGE THIS. IT WILL BREAK THINGS. AutoEvent will automatically manage this setting.")]
-    public virtual string TranslationVersion { get; set; }
+    public virtual string Country { get; set; }
+    public virtual string Version { get; set; }
 }
