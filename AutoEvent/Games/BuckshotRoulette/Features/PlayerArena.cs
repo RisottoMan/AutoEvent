a3a4a3c7f85259571829d2adae37c09efc1057d5
KoT0XleB <96589180+KoT0XleB@users.noreply.github.com>
2024-02-20 12:09:07 +0500

    Create PlayerArena.cs

---
+++ AutoEvent/Games/BuckshotRoulette/Features/PlayerArena.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace AutoEvent.Games.BuckshotRoulette;
+public class PlayerArena
+{
+    public bool IsHaveShotgun { get; set; } = false;
+    public int Kills { get; set; } = 0;
+}
