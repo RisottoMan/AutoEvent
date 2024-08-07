7b9f764c4f1529c57c61eadad6e17d8a51869739
swd <swdchw@gmail.com>
2024-08-07 19:28:22 +0500

    1488 посхалка эщкере 227 Z V

--- AutoEvent/Patches/AddItem.cs
+++ AutoEvent/Patches/AddItem.cs
@@ -1,10 +1,13 @@
-﻿using HarmonyLib;
+﻿using Exiled.API.Enums;
+using Exiled.API.Extensions;
+using HarmonyLib;
 using InventorySystem;
 using InventorySystem.Items;
 using InventorySystem.Items.Firearms;
 using InventorySystem.Items.Firearms.Attachments;
 using InventorySystem.Items.Firearms.Attachments.Components;
 using PluginAPI.Core;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AutoEvent.Patches
@@ -15,10 +18,17 @@ namespace AutoEvent.Patches
         public static bool Prefix(Player __instance, ItemType item, ref ItemBase __result)
         {
             ItemBase itemBase = __instance.ReferenceHub.inventory.ServerAddItem(item, 0);
-            
+
             if (itemBase is Firearm firearm)
             {
                 FirearmStatusFlags firearmStatusFlags = FirearmStatusFlags.MagazineInserted;
+
+                if (AttachmentsServerHandler.PlayerPreferences.TryGetValue(__instance.ReferenceHub, out var preferedAllAttachmets)
+                    && preferedAllAttachmets.TryGetValue(item, out var preferedAttachments))
+                {
+                    firearmStatusFlags |= (FirearmStatusFlags)preferedAttachments;
+                }
+
                 if (firearm.Attachments.Any((Attachment a) => a.Name == AttachmentName.Flashlight))
                 {
                     firearmStatusFlags |= FirearmStatusFlags.FlashlightEnabled;
