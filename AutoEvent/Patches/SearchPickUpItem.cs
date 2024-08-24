7483fca97deb137fd578166655ebbeecfaa9cb9f
KoT0XleB <lapka.roman@yandex.ru>
2024-08-24 12:50:37 +0500

    fixing searchpickupitem patch (x2)

--- AutoEvent/Patches/SearchPickUpItem.cs
+++ AutoEvent/Patches/SearchPickUpItem.cs
@@ -12,9 +12,8 @@ namespace AutoEvent.Patches
     [HarmonyPatch(typeof(SearchCoordinator), nameof(SearchCoordinator.ReceiveRequestUnsafe))]
     internal static class SearchPickUpItemPatch
     {
-        private static bool Prefix(SearchCoordinator __instance, out SearchSession? session, out SearchCompletor completor)
+        private static bool Prefix(SearchCoordinator __instance, ref bool __result, out SearchSession? session, out SearchCompletor completor)
         {
-            DebugLogger.LogDebug("ReceiveRequestUnsafe");
             SearchRequest request = __instance.SessionPipe.Request;
             completor = SearchCompletor.FromPickup(__instance, request.Target, (double) __instance.ServerMaxRayDistanceSqr);
             if (!completor.ValidateStart())
@@ -32,15 +31,16 @@ namespace AutoEvent.Patches
                 completor = null;
                 return true;
             }
+            
             completor = ev.SearchCompletor;
             session = ev.SearchSession;
-            
             SearchSession body = request.Body;
+            
             if (!__instance.isLocalPlayer)
             {
                 double num1 = NetworkTime.time - request.InitialTime;
                 double num2 = (double) LiteNetLib4MirrorServer.Peers[__instance.connectionToClient.connectionId].Ping * 0.001 * __instance.serverDelayThreshold;
-                float num3 = request.Target.SearchTimeForPlayer(__instance.Hub);
+                float num3 = ev.SearchTime;
                 if (num1 < 0.0 || num2 < num1)
                 {
                     body.InitialTime = NetworkTime.time - num2;
@@ -49,7 +49,9 @@ namespace AutoEvent.Patches
                 else if (Math.Abs(body.FinishTime - body.InitialTime - (double) num3) > 0.001)
                     body.FinishTime = body.InitialTime + (double) num3;
             }
-            session = new SearchSession?(body);
+
+            session = null;
+            __result = true;
             return false;
         }
     }
