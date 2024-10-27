66426956a972159478647b0148072c9f30a01392
KoT0XleB <lapka.roman@yandex.ru>
2024-10-27 17:51:48 +0500

    Delete unnecessary games

--- AutoEvent/Games/Fnaf/Features/AnimatronicPreset.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AutoEvent.Games.Fnaf.Features;
-
-public class AnimatronicPreset
-{
-    public string Name { get; set; }
-    public string PositionName { get; set; }
-    public int Level { get; set; }
-    public int Timer { get; set; }
-}
